0347ea17a173894974a72bb00630d852104ee1d5
Willie Pai <PaisWillie@gmail.com>
2023-08-04 16:31:23 -0400

    feat: added static export settings

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@ module.exports = {
   eslint: {
     dirs: ['src'],
   },
+
+  // Deploying Next.js as a Static Site on DigitalOcean
+  output: 'export',
+  distDir: '_static',
+  images: {
+    unoptimized: true,
+  },
+
+  trailingSlash: true,
 };
